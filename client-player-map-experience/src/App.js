104a6b815113800191da42817d9e4a2ca783a52b
nomadic4life <orbison.dennis@gmail.com>
2019-09-15 17:07:48 -0700

    building out the canvas. very early stage of a grid map

--- client-player-map-experience/src/App.js
+++ client-player-map-experience/src/App.js
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function box(ctx, x, y, w, h, line) {
+  ctx.beginPath();
+  ctx.lineWidth = line;
+  ctx.moveTo(x, y);
+  ctx.lineTo(x, y + h);
+  ctx.lineTo(x + w, y + w);
+  ctx.lineTo(x + w, y);
+  ctx.lineTo(x, y);
+  ctx.stroke();
 }
 
+function horizontalExit(ctx, x, y, w, line) {
+  ctx.beginPath();
+  ctx.lineWidth = line;
+  ctx.moveTo(x, y);
+  ctx.lineTo(x + w, y);
+  ctx.stroke();
+}
+
+function VerticalExit(ctx, x, y, w, h, line) {}
+
+function useCanvas() {
+  const canvasRef = React.useRef(null);
+  React.useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    const width = 24;
+    const height = 28;
+    for (let y = 0; y <= height; y++) {
+      for (let x = 0; x <= width; x++) {
+        const line = 2;
+        const w = 26;
+        const h = 26;
+        const sizeX = line + w;
+        const sizeY = line + h;
+        const offset = 15;
+        box(
+          ctx,
+          x * sizeX + (x + 1) * offset,
+          y * sizeY + (y + 1) * offset,
+          w,
+          h,
+          line
+        );
+      }
+    }
+
+    for (let y = 1; y <= height; y++) {
+      for (let x = 1; x <= width; x++) {
+        const size = 15;
+        const line = 6;
+        const offset = 26;
+        horizontalExit(
+          ctx,
+          x * (offset + 2) - 1 + x * size,
+          y * 27 + offset,
+          size,
+          line
+        );
+        VerticalExit(ctx);
+      }
+    }
+  });
+  return [canvasRef];
+}
+
+function App() {
+  const [canvasRef] = useCanvas();
+  // draw(canvasRef);
+  return (
+    <>
+      <canvas
+        ref={canvasRef}
+        width={window.innerWidth - 1}
+        height={window.innerHeight - 4}
+      />
+    </>
+  );
+}
 export default App;
